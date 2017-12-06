f9c6b5c7522536443078bffa0a85a37473a40f91
gmichaud <gmichaud20@gmail.com>
2017-12-06 14:08:37 +0100

    first try

--- includes/linux/rtv1.h
+++ includes/linux/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: gmichaud <gmichaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 09:45:29 by gmichaud          #+#    #+#             */
-/*   Updated: 2017/12/01 12:28:51 by gmichaud         ###   ########.fr       */
+/*   Updated: 2017/12/06 10:39:16 by gmichaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@
 **	Properties
 */
 
-# define WIN_HEIGHT 10
-# define WIN_WIDTH 20
+# define WIN_HEIGHT 600
+# define WIN_WIDTH 600
 # define FOVY 60
 # define COLOR_DEPTH 32
 # define ENDIAN 1
@@ -143,6 +143,7 @@ typedef struct	s_env
 {
 	void		*init;
 	void		*win;
+	t_img		*img;
 }				t_env;
 
 typedef struct	s_args
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gmichaud <gmichaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 09:44:07 by gmichaud          #+#    #+#             */
-/*   Updated: 2017/12/04 13:37:12 by gmichaud         ###   ########.fr       */
+/*   Updated: 2017/12/06 12:10:01 by gmichaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	init_scene(t_scene *scn)
 {
 	t_sphere	sphere;
 
-	sphere.center = ft_init_vec4(0, 0, 0, 1);
+	sphere.center = ft_init_vec4(5, -5, -10, 1);
 	sphere.radius = 1;
 	sphere.color = 0x00FF0000;
 	scn->items = ft_lstnew(&sphere, sizeof(sphere));
@@ -90,6 +90,24 @@ double	check_intersection(t_vec4 dir, t_vec4 orig, t_vec4 ctr, double radius)
 	return (get_distance(a, b, c, disc));
 }
 
+void	put_pixel(int pos, t_img *img, unsigned int color)
+{
+	char	*data;
+	int		width;
+	int		inc;
+
+	data = img->data;
+	inc = img->color_depth / 8;
+	width = img->width; //* (COLOR_DEPTH / 8);
+	ft_memcpy(&data[pos * inc], &color, sizeof(color));
+	//if (pos.x < (width / inc) && pos.y < img->height && pos.x >= 0
+		//&& pos.y >= 0)
+	//{
+			//ft_memcpy(&data[((int)pos.y * width) + ((int)pos.x * inc)], &color,
+				//sizeof(color));
+	//}
+}
+
 int		check_intersections(t_ray ray, t_scene scn)
 {
 	t_sphere	*sph;
@@ -105,32 +123,55 @@ int		check_intersections(t_ray ray, t_scene scn)
 			final = inter;
 		scn.items = scn.items->next;
 	}
-	printf("%f ; ", final);
-	if (final)
+	if (final > 0)
 		return (TRUE);
 	return (FALSE);
 }
 
-int		raytracing(t_ray *ray_list, t_scene scn)
+int		raytracing(t_ray *ray_list, t_scene scn, t_env *env)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < WIN_HEIGHT * WIN_WIDTH)
 	{
-		check_intersections(ray_list[i], scn);
+		if (check_intersections(ray_list[i], scn))
+			put_pixel(i, env->img, 0x00FF0000);
 		++i;
 	}
 	return (0);
 }
 
+int		init_img(t_env	*env)
+{
+	t_img	*img;
+
+	if (!(img = (t_img*)malloc(sizeof(t_img))))
+		return (FAILURE);
+	img->ptr = mlx_new_image(env->init, WIN_WIDTH, WIN_HEIGHT);
+	img->endian = 0;
+	img->width = WIN_WIDTH * COLOR_DEPTH / 8;
+	img->height = WIN_HEIGHT;
+	img->color_depth = COLOR_DEPTH / 8;
+	img->data = mlx_get_data_addr(img->ptr, &img->color_depth,
+		&img->width, &img->endian);
+	env->img = img;
+	return (1);
+}
+
 int		main(void)
 {
+	t_env	env;
 	t_scene scene;
 	t_ray	*ray_list;
 
+	env.init = mlx_init();
+	env.win = mlx_new_window(env.init, WIN_WIDTH, WIN_HEIGHT, "RTV1");
+	init_img(&env);
 	init_scene(&scene);
 	ray_list = create_ray_array(ft_translate(0, 0, 2));
-	raytracing(ray_list, scene);
+	raytracing(ray_list, scene, &env);
+	mlx_put_image_to_window(env.init, env.win, env.img->ptr, 0, 0);
+	mlx_loop(env.init);
 	return (0);
 }
