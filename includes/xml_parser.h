ba5374ad72146105d6dd528fba0e7d319cf66591
Johan GOURDIN <jgourdin@e1r11p22.42.fr>
2018-03-23 12:50:29 +0100

    paufinage

--- includes/xml_parser.h
+++ includes/xml_parser.h
@@ -6,7 +6,7 @@
 /*   By: hbouchet <hbouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 09:45:29 by gmichaud          #+#    #+#             */
-/*   Updated: 2018/03/23 11:54:15 by jgourdin         ###   ########.fr       */
+/*   Updated: 2018/03/23 12:49:06 by jgourdin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 # include "xmlstring.h"
 # include "xmlreader.h"
 
-int		new_triangle(xmlNodePtr node, t_scene *scn, t_triangle triangle);
+int			new_triangle(xmlNodePtr node, t_scene *scn, t_triangle triangle);
 int			create_paraboloid(xmlNodePtr node, t_scene *scn);
 int			create_cone(xmlNodePtr node, t_scene *scn);
 int			create_triangle(xmlNodePtr node, t_scene *scn);
